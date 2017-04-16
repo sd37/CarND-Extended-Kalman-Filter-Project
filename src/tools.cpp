295e4e4582c975cb01a34a1d85f37803d9027f48
spandan <spandan@prosperworks.com>
2017-04-16 22:45:11 +0530

    RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -17,28 +17,28 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     */
 
     VectorXd rmse(4);
-    rmse << 0, 0, 0, 0;
+    rmse << 0,0,0,0;
 
     // check the validity of the following inputs:
     //  * the estimation vector size should not be zero
     //  * the estimation vector size should equal ground truth vector size
-    if (estimations.size() != ground_truth.size()
-        || estimations.size() == 0) {
+    if(estimations.size() != ground_truth.size()
+       || estimations.size() == 0){
         return rmse;
     }
 
     //accumulate squared residuals
-    for (unsigned int i = 0; i < estimations.size(); ++i) {
+    for(unsigned int i=0; i < estimations.size(); ++i){
 
         VectorXd residual = estimations[i] - ground_truth[i];
 
         //coefficient-wise multiplication
-        residual = residual.array() * residual.array();
+        residual = residual.array()*residual.array();
         rmse += residual;
     }
 
     //calculate the mean
-    rmse = rmse / estimations.size();
+    rmse = rmse/estimations.size();
 
     //calculate the squared root
     rmse = rmse.array().sqrt();
@@ -52,7 +52,6 @@ MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
     TODO:
       * Calculate a Jacobian here.
     */
-
     MatrixXd Hj(3, 4);
     //recover state parameters
     float px = x_state(0);
