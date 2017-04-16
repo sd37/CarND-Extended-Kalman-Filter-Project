b3ef16c992f4ac3c384bae45c3094e8aac9295f2
spandan <spandan@prosperworks.com>
2017-04-16 20:20:22 +0530

    update KF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -33,6 +33,7 @@ void KalmanFilter::Update(const VectorXd &z) {
     TODO:
       * update the state by using Kalman Filter equations
     */
+
     VectorXd z_pred = H_ * x_;
     VectorXd y = z - z_pred;
     MatrixXd Ht = H_.transpose();
